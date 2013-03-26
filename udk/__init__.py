d442990be9aa10528ff0cf15370b3dbb565e36fc
Johannes Becker <alfalfasprossen@gmail.com>
2013-03-26 19:48:40 +0100

    export from udk paused, cleaned up some stuff, started working on sync translate object in udk

--- udk/__init__.py
+++ udk/__init__.py
@@ -4,8 +4,9 @@ import udkUI
 import udkTranslator
 import udkCommand
 
-def alive():
-    print("hallo")
+#might simply use from udkCommand import * here but that would make things visible which should not be visible here ;)
+from udkCommand import transformObject
+from udkCommand import setCamera
 
 def connectToInstance():
     """
@@ -13,10 +14,10 @@ def connectToInstance():
     """
     udkUI.connectToUEd()
 
-def setCamera(x,y,z,rx,ry,rz):
-    """
-    set the viewport camera
-    """
-    udkCommand.setCamera(x,y,z,rx,ry,rz)
+#def setCamera(x,y,z,rx,ry,rz):
+#    """
+#    set the viewport camera
+#    """
+#    udkCommand.setCamera(x,y,z,rx,ry,rz)
 
 
