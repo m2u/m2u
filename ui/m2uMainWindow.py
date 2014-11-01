d75d4acffb1e64dbf82555cb2c33b5767f640a81
Johannes Becker <alfalfasprossen@gmail.com>
2014-11-01 03:40:39 +0100

    writing the PySide UI programatically now instead of designer files

---
+++ ui/m2uMainWindow.py
@@ -0,0 +1,103 @@
+"""
+the main window UI
+
+"""
+import m2u
+program = m2u.core.getProgram()
+editor = m2u.core.getEditor()
+
+# we can assume that PySide exists, when this file is loaded
+from PySide import QtCore
+from PySide import QtGui
+from PySide.QtUiTools import QUiLoader
+
+from . import m2uIcons as icons
+
+
+class m2uMainWindow(QtGui.QWidget):
+    def __init__(self, *args, **kwargs):  
+        super(m2uMainWindow, self).__init__(*args, **kwargs)
+        
+        self.setWindowFlags(QtCore.Qt.Window)
+        self.setWindowTitle("m2u "+m2u.getVersion()+" ("
+                            +program.getName()+","+editor.getName()+")")
+        self.setWindowIcon(icons.m2uIcon32)
+        self.buildUI()
+        self.connectUI()
+        self.show()
+    
+    def buildUI(self):
+        
+        # connect row
+        self.topRowLayout = QtGui.QHBoxLayout()
+        self.connectBtn = QtGui.QPushButton(text = "Connect")
+        self.topRowLayout.addWidget(self.connectBtn)
+        self.addressEdit = QtGui.QLineEdit()
+        self.topRowLayout.addWidget(self.addressEdit)
+        self.topRowLayout.addStretch()
+        self.settingsBtn = QtGui.QToolButton()
+        self.settingsBtn.setIcon(icons.icoSettings)
+        self.topRowLayout.addWidget(self.settingsBtn)
+        
+        # sync options checkboxes
+        self.syncOptionsGrp = QtGui.QGroupBox("Sync Whaaat?")
+        layout = QtGui.QGridLayout()
+        self.syncCameraChkbx = QtGui.QCheckBox("Sync Camera")
+        layout.addWidget(self.syncCameraChkbx,0,0)
+        self.syncObjectsChkbx = QtGui.QCheckBox("Sync Objects")
+        layout.addWidget(self.syncObjectsChkbx,1,0)
+        self.syncSelectionChkbx = QtGui.QCheckBox("Sync Selection")
+        layout.addWidget(self.syncSelectionChkbx,1,1)
+        self.syncVisibilityChkbx = QtGui.QCheckBox("Sync Visibility")
+        layout.addWidget(self.syncVisibilityChkbx,2,0)
+        self.syncLayersChkbx = QtGui.QCheckBox("Sync Layers")
+        layout.addWidget(self.syncLayersChkbx,3,0)
+        layout.setColumnStretch(2,1)
+        self.syncOptionsGrp.setLayout(layout)
+        
+        # send and export buttons
+        self.sendGrp = QtGui.QGroupBox("Send/Export")
+        layout = QtGui.QHBoxLayout()
+        layout.setSpacing(1)
+        layout.setContentsMargins(1,1,1,1)
+        self.sendSelBtn = QtGui.QToolButton()
+        self.sendSelBtn.setIcon(icons.m2uIcon128)
+        self.sendSelBtn.setIconSize(QtCore.QSize(64,32))
+        layout.addWidget(self.sendSelBtn)
+        self.sendSelNewBtn = QtGui.QToolButton()
+        self.sendSelNewBtn.setIcon(icons.m2uIcon128)
+        self.sendSelNewBtn.setIconSize(QtCore.QSize(64,32))
+        layout.addWidget(self.sendSelNewBtn)
+        layout.addStretch()
+        self.sendOptionsBtn = QtGui.QToolButton()
+        self.sendOptionsBtn.setIcon(icons.icoSettings)
+        self.sendOptionsBtn.setIconSize(QtCore.QSize(32,32))
+        layout.addWidget(self.sendOptionsBtn)
+        self.sendGrp.setLayout(layout)
+        
+        # add all onto the main form
+        formLayout = QtGui.QVBoxLayout()
+        formLayout.addItem(self.topRowLayout)
+        formLayout.addWidget(self.syncOptionsGrp)
+        formLayout.addWidget(self.sendGrp)
+        #formLayout.setSpacing(1)
+        formLayout.setContentsMargins(1,1,1,1)
+        self.setLayout(formLayout)
+        
+        
+    def connectUI(self):
+        self.connectBtn.clicked.connect( self.connectBtnClicked )
+        
+        #self.syncCameraChkbx.toggled.connect( self.syncCameraChkbxClicked )
+    
+    
+    ################################
+    # Callbacks
+    ################################
+    
+    def connectBtnClicked(self):
+        print "clicked a buttn"
+        
+    
+    def syncCameraChkbxClicked(self, checked):
+        pass
