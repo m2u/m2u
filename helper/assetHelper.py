ea4d37032aa62e3594596c8b2e41bb3d7efbb0a0
Johannes Becker <alfalfasprossen@gmail.com>
2016-09-27 00:07:04 +0200

    Clean up helper module

--- helper/assetHelper.py
+++ helper/assetHelper.py
@@ -1,34 +1,34 @@
 
 
-#class AssetList(object):
-#    pass
-
 class AssetListEntry(object):
-    """convenience class to lighten up asset list complexity during export operations
-    each AssetListEntry stands for a single asset (assetPath) and all the corresponding
-    asset instances (objects). Each instance entry must be a 2tuple of the objects name
-    as a string and an optional program-specific reference to the object, provided
-    for convenience to simplify followup select-operations etc.
+    """Convenience class to lighten up asset list complexity during
+    export operations.
+
+    Each AssetListEntry stands for a single asset (asset_path) and all
+    the corresponding asset instances (objects). Each instance entry
+    must be a 2tuple of the objects name as a string and an optional
+    program-specific reference to the object - provided for convenience.
+    The reference may be used to simplify followup select-operations etc.
     """
-    def __init__(self, assetPath):
-        self.assetPath = assetPath
-        self.objList = []
+    def __init__(self, asset_path):
+        self.asset_path = asset_path
+        self.obj_list = []
 
-    def append(self,t):
-        """ expects tuple of (objName, objRef)
+    def append(self, entry):
+        """Expects tuple of (obj_name, obj_ref)
         """
-        self.objList.append(t)
+        self.objList.append(entry)
 
-    def getExportObject(self):
+    def get_export_object(self):
+        # TOOD: better docstring. What is this used for, why use a func
+        #   for this?
         """return the first object in the list"""
-        return self.objList[0]
+        return self.obj_list[0]
 
-    def getObjectNamesList(self):
-        l = [t[0] for t in self.objList]
-        return l
+    def get_object_names_list(self):
+        names = [entry[0] for entry in self.obj_list]
+        return names
 
-    def getObjectReferencesList(self):
-        l = [t[1] for t in self.objList]
-        return l
-        
-        
+    def get_object_references_list(self):
+        references = [entry[1] for entry in self.obj_list]
+        return references
--- helper/systemHelper.py
+++ helper/systemHelper.py
@@ -1,45 +1,47 @@
-""" helper functions for mainly operating system stuff """
+"""Helper functions for mainly operating system stuff """
 
 import os
 import time
+import logging
+
+_lg = logging.getLogger(__name__)
 
 
-def waitForFileToBecomeAvailable(path,maxWaitTime=10):
+def wait_for_file_to_become_available(path, timeout_seconds=10):
     """
     Wait till a specified file is available for further processing.
 
     This means it waits till the file comes into existence and no other
     application is using the file (as far as we can determine it). This
-    function is important for stuff like telling UDK to export something and
-    importing it into the 3d-application in the next step.
-    
-    The function will return True when the file is available, and False if the
-    maxWaitTime has passed before the file is available.
-    
+    function is important for stuff like telling UDK to export something
+    and importing it into the 3d-application in the next step.
+
+    The function will return True when the file is available, and False
+    if the timeout_seconds have passed before the file is available.
+
     """
-    # this is a better approach than trying to open the file, because we could
-    # open it although it was in use. We now try to rename it. Windows will
-    # give us an exception if the file is not available and, when it is
-    # available, if the file is in use by another process.
-    # This is exactly what we want ;)
-    
-    # make sure there is nothing blocking from a failed previous attempt
+
+    # To determine if the file is still in use, we try to rename it.
+    # Windows will give us an exception if the file is not available
+    # and, when it is available, if the file is in use by another
+    # process.
+
+    # Make sure there is nothing blocking from a failed previous attempt.
     TOUCHED_STR = "touched"
-    if os.path.exists(path+TOUCHED_STR):
-        os.remove(path+TOUCHED_STR) 
-    
-    waitedTime = 0
-    # now try to rename the file we are waiting for until we are able to do so
+    renamed_file_path = path + TOUCHED_STR
+    if os.path.exists(renamed_file_path):
+        os.remove(renamed_file_path)
+
+    waited_time = 0
     while True:
         try:
-            os.rename(path, path+TOUCHED_STR)
-            os.rename(path+TOUCHED_STR, path)
-            return True # file is available for us
-        except OSError as e:
-            #print e
+            os.rename(path, renamed_file_path)
+            os.rename(renamed_file_path, path)
+            return True  # File is available for us
+        except OSError:
             time.sleep(0.01)
-            waitedTime += 0.01
-            if waitedTime > maxWaitTime:
-                print "# m2u: Waited for too long for file to become available: "+path
+            waited_time += 0.01
+            if waited_time > timeout_seconds:
+                _lg.error("Waited for too long for file to become available: "
+                          "{0}".format(path))
                 return False
-    
