b82f3ccf2badf157c0fbf38b4df65f7b1a613447
Johannes Becker <alfalfasprossen@gmail.com>
2018-01-20 14:52:57 +0100

    Add real ue4 tests

--- ue4/tests/test_messaging.py
+++ ue4/tests/test_messaging.py
@@ -3,6 +3,7 @@ import pytest
 from m2u.ue4 import connection
 
 
+@pytest.mark.ue4
 def test_send_message_size():
     """Send a big message, larger than buffer size, so the server has to
     read multiple chunks.
@@ -13,3 +14,8 @@ def test_send_message_size():
     result = connection.send_message(message)
     assert result == str(len(message))
     connection.disconnect()
+
+
+def test_send_message_no_connection():
+    result = connection.send_message("")
+    assert result is None
